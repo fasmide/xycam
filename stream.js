b3dd266204a7db662a6e238eeb05d0a39911d6f2
Kristian Mide <fas@mide.dk>
2015-07-07 21:21:35 +0200

    Some moving around in stream.js

--- stream.js
+++ stream.js
@@ -3,15 +3,21 @@ var MjpegConsumer = require("mjpeg-consumer");
 var WebSocket = require('ws');
 var SerialPort = require("serialport")
 
-var ws = new WebSocket('ws://localhost:3000/imgReceive');
 
-var controlWs = new WebSocket('ws://localhost:3000/control');
+
 
 var serialPort = new SerialPort.SerialPort("/dev/ttyUSB0", {
   baudrate: 115200,
   parser: SerialPort.parsers.readline("\n")
 });
 
+
+
+//
+// Image socket
+//
+//
+var ws = new WebSocket('ws://localhost:3000/imgReceive');
 
 var imgs = 0;
 ws.on('open', function() {
@@ -26,33 +32,52 @@ ws.on('open', function() {
 
 	//request("http://10.0.0.1:60152/liveview.JPG?%211234%21http%2dget%3a%2a%3aimage%2fjpeg%3a%2a%21%21%21%21%21").pipe(consumer);
 
-
 });
+
+setInterval(function() {
+	process.stdout.write("\rImg/sec: "+ imgs);
+	imgs = 0;
+}, 1000);
+
+
+//
+// Control socket
+// for positioning and reporting
+//
+var controlWs = new WebSocket('ws://localhost:3000/control');
 
 controlWs.on('open', function() {
 	console.log("Control socket opened");
 
 });
 
+
+//
+// Stepper
+// uses slutty protocol like:
+//
+// Jogging:
+//		j10<space>10<space>
+//		- Moves +10 x, y relative to current position
+//
+// Absolute position:
+// 		g10<space>10<space>
+//		- Moves to position 10x 10y
+//
+
 serialPort.on("open", function () {
   console.log('Serialport opened');
-  serialPort.write('?');
 });
 
 serialPort.on('data', function(str) {
-	console.log('Has serial data:', str);
-	if (str.match(/^Grbl/)) {
-		console.log("grbl ready");
-		setInterval(function() {
-			serialPort.write('?');
-		}, 250);
-		return;
-	}
-	controlWs.send(str);
+	
+	var data = str.split(' ');
+	controlWs.send(JSON.stringify({
+		currentX: parseInt(data[0], 10),
+		currentY: parseInt(data[1], 10),
+		targetX: parseInt(data[2], 10),
+		targetY: parseInt(data[3], 10),
+		distanceX: parseInt(data[4], 10),
+		distanceY: parseInt(data[5], 10)
+	}));
 });
-
-
-setInterval(function() {
-	process.stdout.write("\rImg/sec: "+ imgs);
-	imgs = 0;
-}, 1000);
